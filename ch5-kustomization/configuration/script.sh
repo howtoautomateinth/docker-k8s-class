1f984a3592b1115384e78df3bd2be901e613163a
HowToAutomate.in.th <mart@howtoautomate.in.th>
2020-01-21 13:13:56 +0700

    Example usage of kustomization tool (management of Kubernetes objects)

---
+++ ch5-kustomization/configuration/script.sh
@@ -0,0 +1,11 @@
+# Create a application.properties file
+cat <<EOF >application.properties
+FOO=Bar
+EOF
+
+cat <<EOF >./kustomization.yaml
+configMapGenerator:
+- name: example-configmap-1
+  files:
+  - application.properties
+EOF
