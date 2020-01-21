efa548acee9df3d3ffed944ebc73cfb4b7c02f21
HowToAutomate.in.th <mart@howtoautomate.in.th>
2020-01-21 14:16:41 +0700

    Added simple usage of generatorOption that control configMap and Secret generator

---
+++ ch5-kustomization/generator/script.sh
@@ -0,0 +1,12 @@
+cat <<EOF >./kustomization.yaml
+configMapGenerator:
+- name: example-configmap-3
+  literals:
+  - FOO=Bar
+generatorOptions:
+  disableNameSuffixHash: true
+  labels:
+    type: generated
+  annotations:
+    note: generated
+EOF
